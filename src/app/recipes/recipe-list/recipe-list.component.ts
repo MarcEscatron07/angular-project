15b1adbe2bd0c5a4b161b12e8d2b7b0a7d902bc6
Marc Benedict C. Escatron <marc.escatron07@gmail.com>
2021-01-08 16:24:40 +0800

    partially finished the Recipe section of the project

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../models/recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,10 +7,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('recipe1', '1st test Recipe', 'https://c.pxhere.com/images/15/3d/9ee477ee62341b9480ce314b02f8-1417897.jpg!d'),
+    new Recipe('recipe2', '2nd test Recipe', 'https://get.pxhere.com/photo/dish-meal-food-colorful-meat-barbecue-cuisine-onion-bacon-asian-food-tasty-skewer-edible-kebab-grilling-mediterranean-food-hors-d-oeuvre-red-green-shish-kebab-grilled-food-meat-skewer-edible-smoked-bacon-brochette-shashlik-695329.jpg'),
+    new Recipe('recipe3', '3rd test Recipe', 'https://get.pxhere.com/photo/dish-meal-food-meat-cuisine-taco-carnitas-203331.jpg'),
+  ];
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  onAddNewRecipe = () => {
+    console.log("onAddNewRecipe() is called!");
+  }
 }

9a70bda0429591e16c1ec27b04c4bf89edf8d7fa
Marc Benedict C. Escatron <marc.escatron07@gmail.com>
2021-01-08 19:39:30 +0800

    finished Section 3: Course Project - The Basics

---
+++ src/app/data-models/ingredient.model.ts
@@ -0,0 +1,18 @@
+export class Ingredient {
+    // // normal declaration
+    // public name: string;
+    // public amount: number;
+
+    // constructor(name: string, amount: number){
+    //     this.name = name;
+    //     this.amount = amount;
+    // }
+    // // 
+
+    // shorthand declaration
+    constructor(
+        public name: string,
+        public amount: number
+    ) { }
+    // 
+}
